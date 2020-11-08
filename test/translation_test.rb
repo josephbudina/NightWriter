bed3e14d3ba2c63223bc6de3baf357ed61cf6a31
Joseph Budina <joebud3@gmail.com>
2020-11-08 15:32:42 -0700

    Add it exists and alphabet tests

---
+++ test/translation_test.rb
@@ -0,0 +1,22 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/translation'
+
+class TranslationTest < Minitest::Test
+  def setup
+    text = './message.txt'
+    message = File.open(text, "r")
+    incoming_message = message.read
+    @translation = Translation.new(incoming_message)
+  end
+
+  def test_it_exists_with_attributes
+    assert_instance_of Translation, @translation
+  end
+  
+  def test_alphabet
+  expected = ["0.", "..", ".."]
+
+  assert_equal expected, @translation.alphabet["a"]
+  end
+end
