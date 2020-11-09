d24c8134c35b3e366d696845fce78c9001f295e3
Joseph Budina <joebud3@gmail.com>
2020-11-09 16:40:24 -0700

    Add translate words and group braille methods

--- lib/translation.rb
+++ lib/translation.rb
@@ -62,4 +62,27 @@ class Translation
   def render_text(text)
     translate(text).each_slice(40).to_a
   end
+
+  #translate from bralle to english as well
+  def translate_words(text)
+    letters = []
+    translate(text).each_slice(3) do |letter|
+       letters << letter
+    end
+    letters
+  end
+
+  def group_braille(text)
+    top = []
+    middle = []
+    bottom = []
+    lines = []
+    translate_words(text).each do |letter|
+      top << letter[0]
+      middle << letter[1]
+      bottom << letter[2]
+      lines = top + ["\n"] + middle+ ["\n"] + bottom
+    end
+    lines.join
+  end
 end
